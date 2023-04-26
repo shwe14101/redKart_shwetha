1d3825675e8616392b0dafec6828cb4fbdca21e7
shwe14101 <76464543+shwe14101@users.noreply.github.com>
2023-04-26 17:17:58 +0530

    Update Homepage.jsx

--- frontend/src/Components/Home/Homepage.jsx
+++ frontend/src/Components/Home/Homepage.jsx
@@ -5,9 +5,6 @@ import Filterss from './Filters';
 import {Filter} from '../Filter/Filterfunctions/Filter'
 import Paginationn from './pagination';
 import { Link } from 'react-router-dom';
-//import Pagen from './pagen';
-import  {FilterProvider}  from '../Filter/filtercontext';
-
 import ProductCard from './prdcard';
 import Productdisplay from './productsdisplay';
 
